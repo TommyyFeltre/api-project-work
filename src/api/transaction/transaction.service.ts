1c87cd28f82299d0c129523b753ba7f4ab6bcef8
TommyyFeltre <tommaso.feltrin@allievi.itsdigitalacademy.com>
2023-09-21 16:20:56 +0200

    find last transaction

--- src/api/transaction/transaction.service.ts
+++ src/api/transaction/transaction.service.ts
@@ -1,11 +1,24 @@
+import { NotFoundError } from "../../errors/not-found";
 import { Transaction } from "./transaction.entity";
 import { Transaction as TransactionModel } from "./transaction.model";
 
 export class TransactionService {
-    async add(transaction: Transaction) {
+    async add(transaction: Transaction): Promise<Transaction> {
         const newTransaction = await TransactionModel.create({ ...transaction });
         await newTransaction.populate('bankAccount category');
         return newTransaction;
+    }
+
+    private async findLastTrans(bankAccount: string): Promise<Transaction> {
+        const lastTransaction = await TransactionModel.findOne({ bankAccount })
+            .sort({ date: -1 })
+            .limit(1)
+            .populate('bankAccount category')
+        if(lastTransaction) {
+            return lastTransaction;
+        } else {
+            throw new NotFoundError();
+        }
     }
 }
 
