0a23562417404282ab58b2a6842cf1f496f8a948
TommyyFeltre <tommaso.feltrin@allievi.itsdigitalacademy.com>
2023-09-21 09:31:34 +0200

    db connection method

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@ import 'reflect-metadata';
 
 import app from './app';
 import mongoose from 'mongoose';
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
 
 mongoose.set('debug', true);
-mongoose.connect('mongodb://127.0.0.1:27017/sim-todo')
+mongoose.connect(process.env.DB_URI!)
   .then(_ => {
     console.log('Connected to db');
-    app.listen(3000, () => {
-      console.log('Server listening on port 3000');
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
     });
   })
   .catch(err => {
