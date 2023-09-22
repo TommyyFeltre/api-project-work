2923cd842b3fb58fd7db76eb70e5a3476775a0b8
TommyyFeltre <tommaso.feltrin@allievi.itsdigitalacademy.com>
2023-09-22 17:00:25 +0200

    router fixed

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import authRouter from './auth/auth.router';
+import categoryRouter from './category/category.router';
+import transactionRouter from './transaction/transaction.router';
 
 const router = Router();
 
 router.use(authRouter);
+router.use('/category', categoryRouter);
+router.use('/transaction', transactionRouter);
 
 export default router;
