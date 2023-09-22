fc3be27d7820d8a492369000f8d5571f1edb920f
Filippofr <filippo.fr7@gmail.com>
2023-09-22 16:20:41 +0200

    autenticatione finita

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,12 +1,18 @@
 import { NextFunction, Response } from "express";
 import { TypedRequest } from "../../utils/typed-request.interface";
-import { AddUserDTO, LoginDTO } from "./auth.dto";
+import { AddUserDTO, LoginDTO, ResetPasswordDTO } from "./auth.dto";
 import { omit, pick } from 'lodash';
 import { UserExistsError } from "../../errors/user-exists";
 import userService from '../user/user.service';
 import passport from "passport";
 import * as jwt from 'jsonwebtoken';
 import bankAccountService from "../bank-account/bank-account.service";
+import { Strategy as LocalStrategy } from "passport-local";
+import { UserIdentity } from "../../utils/auth/local/user-identity.model";
+import * as bcrypt from 'bcrypt';
+import { User as UserModel } from "../user/user.model";
+import { WrongPasswordError } from "../../errors/wrong-password";
+
 
 const JWT_SECRET = 'my_jwt_secret';
 
@@ -41,6 +47,7 @@ export const login = async (
     if (err) {
       return next(err);
     }
+    console.log(user)
     if (!user) {
       res.status(401);
       res.json({
@@ -56,4 +63,44 @@ export const login = async (
       token
     });
   })(req, res, next);
+}
+
+export const resetPassword = async (
+  req: TypedRequest<ResetPasswordDTO>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId = req.user!.id;
+    const { oldPassword, newPassword } = req.body;
+
+    if (oldPassword === newPassword) {
+      res.status(400);
+      res.json({
+        error: 'PasswordValidationError',
+        message: 'New password must be different from the last one',
+      });
+      return;
+    }
+    if (!req.user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const modifiedUser = await userService.update(userId!, newPassword, oldPassword);
+
+
+    res.status(200);
+    res.json({
+      modifiedUser,
+      message: 'Password changed'
+    });
+  } catch (err) {
+    if (err instanceof WrongPasswordError) {
+      res.status(400);
+      res.send(err.message);
+    } else {
+      next(err);
+    }
+  }
+    
 }
--- src/errors/not-found.ts
+++ src/errors/not-found.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from "express";
-import { UserExistsError } from "./user-exists";
 
 export class NotFoundError extends Error {
   constructor() {
@@ -11,16 +10,6 @@ export class NotFoundError extends Error {
 
 export const notFoundHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof NotFoundError) {
-    res.status(404);
-    res.json({
-      error: err.name,
-      message: err.message
-    });
-  } else {
-    next(err);
-  }
-
-  if (err instanceof UserExistsError) {
     res.status(404);
     res.json({
       error: err.name,
